ad608faa1145b841edde38324064afb842d0104f
晓破千年 <qq_62228785@gitcode.net>
2024-11-14 14:48:03 +0800

    feat.程序退出关闭浏览器

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import threading
+
+import my_logger
+from Chrome import Chrome
 from appoitment import appointment, login
 from art import *
 
+
+
 if __name__ == '__main__':
     tprint("SDUT-APPOINTMENT")
-    appointment(login())
+    try:
+        appointment(login())
+    finally:
+        my_logger.MYLogger().logger.info("正在关闭浏览器...")
+        Chrome().get_browser().quit()
+        my_logger.MYLogger().logger.info("浏览器已经关闭")
