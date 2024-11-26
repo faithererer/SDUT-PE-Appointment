9eeea4a46d2688cf331eaf65f39936f814b8ac0f
晓破千年 <qq_62228785@gitcode.net>
2024-11-26 21:49:55 +0800

    update. 同步域名信息

--- CONSTRANTS.py
+++ CONSTRANTS.py
@@ -1,27 +1,30 @@
 # sdut认证url
+
+HOST = 'http://tcenter.sdut.edu.cn/'
+
 AUTH_URL = 'http://10-17-27-11.newvpn.sdut.edu.cn:8118/'
 
 AUTH_LOGOUT_URL = 'https://newvpn.sdut.edu.cn/passport/v1/user/logout?clientType=SDPBrowserClient&platform=Windows&lang=zh-CN'
 
-NOTICE_URL = 'http://10-17-27-11.newvpn.sdut.edu.cn:8118/api/interinfo/notice/toList?sf_request_type=ajax'
+NOTICE_URL = HOST+'/api/interinfo/notice/toList?sf_request_type=ajax'
 
-INNER_URL = 'http://10-17-27-11.newvpn.sdut.edu.cn:8118/#/test/1'
-OUTER_URL = 'http://10-17-27-11.newvpn.sdut.edu.cn:8118/#/test/2'
+INNER_URL = HOST+'/#/test/1'
+OUTER_URL = HOST+'/#/test/2'
 
 AP_LIST = "/api/tzjc/testappointment/get-appointment-date-list"
 
-APP_DATE_LIST = 'http://10-17-27-11.newvpn.sdut.edu.cn:8118/api/tzjc/testappointment/get-appointment-time-list?sf_request_type=ajax'
-APP_TIME_PERIOD = 'http://10-17-27-11.newvpn.sdut.edu.cn:8118/api/tzjc/testappointment/get-appointment-time-type?sf_request_type=ajax'
-AP_APPLY = "http://10-17-27-11.newvpn.sdut.edu.cn:8118/api/tzjc/testappointment/apply?sf_request_type=ajax"
-APP_LIST = 'http://10-17-27-11.newvpn.sdut.edu.cn:8118/api/tzjc/testappointment/get-appointment-date-list?sf_request_type=ajax'
-AP_LOGIN = 'http://10-17-27-11.newvpn.sdut.edu.cn:8118/api/sys/user/bind/wechat?sf_request_type=ajax'
+APP_DATE_LIST = HOST+'/api/tzjc/testappointment/get-appointment-time-list?sf_request_type=ajax'
+APP_TIME_PERIOD = HOST+'/api/tzjc/testappointment/get-appointment-time-type?sf_request_type=ajax'
+AP_APPLY = HOST+'/api/tzjc/testappointment/apply?sf_request_type=ajax'
+APP_LIST = HOST+'/api/tzjc/testappointment/get-appointment-date-list?sf_request_type=ajax'
+AP_LOGIN = HOST+'/api/sys/user/bind/wechat?sf_request_type=ajax'
 WARN1 = '距离上次预约时间未超50天，无法继续预约，如有疑问，请联系相关老师!'
 
 
 
 cookies = {
-    'platformMultilingual_-_edu.cn': 'zh_CN',
-    'sdp_user_token': '',
+    # 'platformMultilingual_-_edu.cn': 'zh_CN',
+    # 'sdp_user_token': '',
 }
 
 headers = {
@@ -30,10 +33,10 @@ headers = {
     'Cache-Control': 'no-cache',
     'Content-Type': 'application/json',
     # 'Cookie': 'platformMultilingual_-_edu.cn=zh_CN; sdp_user_token=4a07fdd7-cc46-4c9d-a037-e005599422a2_dabfa38a-c70e-4610-9a56-d656b106d932',
-    'Origin': 'http://10-17-27-11.newvpn.sdut.edu.cn:8118',
+    'Origin': HOST,
     'Pragma': 'no-cache',
     'Proxy-Connection': 'keep-alive',
-    'Referer': 'http://10-17-27-11.newvpn.sdut.edu.cn:8118/',
+    'Referer': HOST,
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
 }
 
